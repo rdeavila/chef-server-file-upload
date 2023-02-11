10058993666918332ecf67b7e235676c7bd3faa4
Rodrigo de Avila <rdeavila@gmail.com>
2023-02-11 17:48:56 -0300

    Add cookbook upload functionality

--- README.md
+++ README.md
@@ -1,9 +1,14 @@
 # Chef Server File Upload
 
 The Chef Server File Upload extension is a powerful tool for developers using
-Visual Studio Code. It provides an effortless way to upload `.json` files to a
-Chef Server, making it easier to manage your infrastructure. Whether you're a
+Visual Studio Code. It provides an effortless way to upload `.json` files
+(nodes, roles, environments, databags, policyfiles) and cookbooks to a Chef
+Server, making it easier to manage your infrastructure. Whether you're a
 seasoned chef or just starting out, the Chef Server File Upload extension is an
 indispensable tool that will make your life easier.
+
+*This extension only works on preconfigured chef repos.* Look for a
+`.chef-repo.txt` file on your repo, and if `knife` command is working before
+using this extension.
 
 ![Send the opened file to Chef Server](images/screenshot.png "Menu item")
--- src/extension.ts
+++ src/extension.ts
@@ -1,22 +1,6 @@
 import * as vscode from 'vscode';
 import * as cp from "child_process";
 
-function getActiveEditorRelativePath(): string | undefined {
-	const editor = vscode.window.activeTextEditor;
-	if (!editor) {
-	  return undefined;
-	}
-	return vscode.workspace.asRelativePath(editor.document.fileName, false);
-}
-
-function getWorkspaceRoot(): string | undefined {
-	const workspaceFolders = vscode.workspace.workspaceFolders;
-	if (!workspaceFolders || !workspaceFolders.length) {
-	  return undefined;
-	}
-	return workspaceFolders[0].uri.fsPath;
-}
-
 export function activate(context: vscode.ExtensionContext) {
 	const execShell = (cmd: string) =>
 	  new Promise<string>((resolve, reject) => {
@@ -31,18 +15,59 @@ export function activate(context: vscode.ExtensionContext) {
 	context.subscriptions.push(
 	  vscode.commands.registerCommand('chef-server-file-upload.runKnifeUpload', async () => {
 		await execShell('knife upload '+getActiveEditorRelativePath());
+	  }),
+	  vscode.commands.registerCommand('chef-server-file-upload.runKnifeCookbookUpload', async () => {
+		await execShell('knife cookbook upload '+getActiveEditorCookbookName());
 	  })
 	);
 
-	let disposable = vscode.commands.registerCommand('chef-server-file-upload.uploadFile', () => {
-		if (getActiveEditorRelativePath() === undefined ) {
-			vscode.window.showErrorMessage("There's no opened file to upload.");
-		} else {
-			vscode.commands.executeCommand('chef-server-file-upload.runKnifeUpload');
-		}
-	});
+	context.subscriptions.push(
+		vscode.commands.registerCommand('chef-server-file-upload.file', () => {
+			if (getActiveEditorRelativePath() === undefined ) {
+				vscode.window.showErrorMessage("There's no opened file to upload.");
+			} else {
+				vscode.commands.executeCommand('chef-server-file-upload.runKnifeUpload');
+			}
+		}),
+		vscode.commands.registerCommand('chef-server-file-upload.cookbook', () => {
+			if (getActiveEditorCookbookName() === undefined ) {
+				vscode.window.showErrorMessage("This file doesn't belong to a cookbook.");
+			} else {
+				vscode.commands.executeCommand('chef-server-file-upload.runKnifeCookbookUpload');
+			}
+		})
+	);
+}
 
-	context.subscriptions.push(disposable);
+function getActiveEditorRelativePath(): string | undefined {
+	const editor = vscode.window.activeTextEditor;
+	if (!editor) {
+	  return undefined;
+	}
+	return vscode.workspace.asRelativePath(editor.document.fileName, false);
+}
+
+function getActiveEditorCookbookName(): string | undefined {
+	const editor = vscode.window.activeTextEditor;
+	if (!editor) {
+	  return undefined;
+	}
+
+	const cookbookPath = vscode.workspace.asRelativePath(editor.document.fileName, false).split('/');
+
+	if (cookbookPath[0] === 'cookbooks') {
+		return cookbookPath[1];
+	} else {
+		return undefined;
+	}
+}
+
+function getWorkspaceRoot(): string | undefined {
+	const workspaceFolders = vscode.workspace.workspaceFolders;
+	if (!workspaceFolders || !workspaceFolders.length) {
+	  return undefined;
+	}
+	return workspaceFolders[0].uri.fsPath;
 }
 
 export function deactivate() {}
